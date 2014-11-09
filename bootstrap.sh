7338c0593f9b756ef6b27ebff253ce0fe30b5af4
root <hugh.grigg@gmail.com>
2014-11-09 21:35:18 +0000

    OP-8 Set up basic Vagrant box to serve app

---
+++ bootstrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+add-apt-repository -y ppa:nginx/stable
+apt-get update
+apt-get install -qq nginx
+
+rm /etc/nginx/sites-enabled/default
+
+echo """server {
+    listen 80;
+    server_name outpost.dev;
+    root /vagrant/app;
+    index index.html;
+}""" > /etc/nginx/sites-available/outpost.dev
+
+ln -s /etc/nginx/sites-available/outpost.dev /etc/nginx/sites-enabled/outpost.dev
+
+service nginx restart
+
+echo "Finished provisioning"
